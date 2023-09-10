bce9ca8d163427b9361200cd5f777daa80f80e87
ys0222 <ggdbstjd0222@gmail.com>
2023-09-11 02:34:28 +0900

    Chore : 빌드 파일 수정

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react';
 
 import Board from './pages/board/Board';
 import BoardDetail from './pages/board/BoardDetail';
+import Link from './pages/Main';
 
 import { Route , Routes } from 'react-router';
 
 function App() {
+
   return (
+    
     <Routes>
+      <Route path='/' element={<Link /> } />
 
       <Route path='/board' element={<Board /> } />
       <Route path='/boardDetail' element={<BoardDetail /> } />
---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export default function Main(){
+    return(
+        <>
+        
+        
+            <Link to ='/board' >보드 </Link>
+            <Link to ='/boardDetail' >보드데테일 </Link>
+
+            
+        </>
+            
+    )
+}

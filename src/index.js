0d39660cf9e86bec0c35a2df71ea148074b0add8
ys0222 <ggdbstjd0222@gmail.com>
2023-09-11 02:07:14 +0900

    Chore : 빌드 파일 수정

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { GlobalStyle } from './styles/global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <GlobalStyle />
     <App />
   </BrowserRouter>
